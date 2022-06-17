47dbc5a68937b2ee6b3761e3fc763b728ca46277
Yingqi Huang <yingqi.huang@thoughtworks.com>
2022-06-17 17:54:22 +0800

    first commit

---
+++ src/main/java/com/example/todoappspringwithangular/TodoAppSpringWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappspringwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppSpringWithAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppSpringWithAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todoappspringwithangular/TodoAppSpringWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappspringwithangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppSpringWithAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
